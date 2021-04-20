1e2a67ce4bca4861d09196ae6ec3f2f657e9b42c
Akimeo <akim.parinoff@gmail.com>
2021-04-20 17:17:27 +0300

    Blocks added

--- cargo_car.rb
+++ cargo_car.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
 class CargoCar < Car
-  def initialize
+  attr_reader :volume, :occupied_volume
+
+  def initialize(number, volume)
+    super(number)
     @type = :cargo
+    @volume = volume
+    @occupied_volume = 0
   end
+
+  def take_volume(amount)
+    return if occupied_volume + amount > volume
+
+    self.occupied_volume += amount
+  end
+
+  def free_volume
+    volume - occupied_volume
+  end
+
+  private
+
+  attr_writer :occupied_volume
 end
